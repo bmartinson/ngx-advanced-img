b4441cebf6ded5edba905b7181e22e70574ca61e
bmartinson <brian@brianmartinson.com>
2024-07-26 21:02:03 -0700

    update test case

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -16,7 +16,7 @@ export class AppComponent {
 
       let mimeType: string = 'image/jpeg';
       let quality = 1;
-      let sizeLimit = 2097152; // 6291456 for 6MB -- try 2097152 for 2MB
+      let sizeLimit = 3355443.2; // 6291456 for 6MB -- try 2097152 for 2MB -- try 3355443.2 for 3.2MB
       let resizeFactor = 1;
       let url = '';
 
