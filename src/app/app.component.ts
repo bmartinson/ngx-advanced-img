21b55022956c93506f34b441df35faa52781be63
bmartinson <brian@brianmartinson.com>
2024-07-29 12:27:36 -0700

    Remove Old Code

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -86,16 +86,4 @@ export class AppComponent {
     });
   }
 
-  private arrayBufferToBase64(buffer: ArrayBuffer): string {
-    let binary = '';
-    const bytes = new Uint8Array(buffer);
-    let len = bytes.byteLength;
-
-    for (let i = 0; i < len; i++) {
-      binary += String.fromCharCode(bytes[i]);
-    }
-
-    return window.btoa(binary);
-  }
-
 }
