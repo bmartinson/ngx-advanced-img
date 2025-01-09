9c52304b168a79b8469b1841cedfbfa3c07d0200
Kyle Burt <kyle@treering.com>
2025-01-09 10:05:28 -0800

    add type def file

---
+++ projects/ngx-advanced-img/src/lib/types/libheif.d.ts
@@ -0,0 +1,22 @@
+declare module 'libheif-js/wasm-bundle' {
+	interface DecodeResult {
+		img: {
+			is_primary: boolean;
+			thumbnails: number;
+			width: number;
+			height: number;
+		} | null;
+		get_width: () => number;
+		get_height: () => number;
+		is_primary: () => boolean;
+		display: (
+			base: ImageData,
+			callback: (result: ImageData | null) => void
+		) => void;
+	}
+
+	type DecodeResultType = DecodeResult[];
+	class HeifDecoder {
+		decode(buffer: ArrayBuffer): DecodeResultType;
+	}
+}
