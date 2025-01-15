ac2fd3cb01ec0e6fb2cd59d2a01cedf6916f389e
bmartinson <brian@brianmartinson.com>
2025-01-15 12:03:24 -0800

    Fix Compilation

--- projects/ngx-advanced-img/src/lib/ngx-advanced-img.module.ts
+++ projects/ngx-advanced-img/src/lib/ngx-advanced-img.module.ts
@@ -1,10 +1,8 @@
 import { NgModule } from '@angular/core';
 
-import { NgxAdvancedImgFallbackDirective } from './directives/ngx-advanced-img-fallback.directive';
-
 @NgModule({
-  declarations: [NgxAdvancedImgFallbackDirective],
+  declarations: [],
   imports: [],
-  exports: [NgxAdvancedImgFallbackDirective],
+  exports: [],
 })
 export class NgxAdvancedImgModule {}
