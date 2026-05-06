f4f3f8efe5734eb17533cf0d2f862b9685d098ac
bmartinson <brian@brianmartinson.com>
2026-05-05 17:59:00 -0700

    Updates

--- eslint.config.js
+++ eslint.config.js
@@ -2,7 +2,7 @@
 const eslint = require('@eslint/js');
 const tseslint = require('typescript-eslint');
 const angular = require('angular-eslint');
-const esImport = require('eslint-plugin-import');
+const esImport = require('eslint-plugin-import-x');
 
 module.exports = tseslint.config(
   {
